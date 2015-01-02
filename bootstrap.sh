08f61f3057e3bfbea1843ed7ecd9d0cf3c9ce4fa
Andy Holt <andrew.holt@hotmail.co.uk>
2015-01-02 20:46:32 +0000

    Add some debugging output from bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,24 +4,27 @@
 
 echo -e "Setting up dotfiles:"
 
-DOTFILES_DIR=$(pwd)
+# get the directory of this script.
+DOTFILES_DIR="$( cd \"$( dirname \"$0\" )\" && pwd)"
+echo $DOTFILES
+echo $HOME
 
 # add zsh prompt to oh-my-zsh for autoloading
 cp $DOTFILES_DIR/adh.zsh-theme $DOTFILES_DIR/oh-my-zsh/themes/.
 
 # bash config
-ln -s $DOTFILES_DIR/bash_aliases ~/.bash_aliases
-ln -s $DOTFILES_DIR/bash_logout ~/.bash_logout
-ln -s $DOTFILES_DIR/bashrc ~/.bashrc
+ln -s $DOTFILES_DIR/bash_aliases $HOME/.bash_aliases
+ln -s $DOTFILES_DIR/bash_logout $HOME/.bash_logout
+ln -s $DOTFILES_DIR/bashrc $HOME/.bashrc
 
 # terminator config
-ln -s $DOTFILES_DIR/terminator_config ~/.config/terminator/config
+ln -s $DOTFILES_DIR/terminator_config $HOME/.config/terminator/config
 
 # tmux config
-ln -s $DOTFILES_DIR/tmux.conf ~/.tmux.conf
+ln -s $DOTFILES_DIR/tmux.conf $HOME/.tmux.conf
 
 # zsh config
-ln -s $DOTFILES_DIR/zsh_aliases ~/.zsh_aliases
-ln -s $DOTFILES_DIR/zshrc ~/.zshrc
+ln -s $DOTFILES_DIR/zsh_aliases $HOME/.zsh_aliases
+ln -s $DOTFILES_DIR/zshrc $HOME/.zshrc
 
 echo -e "Set up of dotfiles complete."
