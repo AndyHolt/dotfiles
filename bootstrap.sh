13978753e13cc5ea82cfc2eee74a20b481ee35f6
Andy Holt <andrew.holt@hotmail.co.uk>
2015-01-02 22:21:00 +0000

    Allow passing of user name argument to bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,31 +2,38 @@
 
 # Set up dotfiles to standard locations (mostly in home directory)
 
+# If input argument is passed, use that as user directory name
+if [ "$1" != "" ]; then
+    HOME_DIR="$HOME"
+else
+    HOME_DIR="/home/$1"
+fi
+
 echo -e "Setting up dotfiles:"
 
 # get the directory of this script.
 DOTFILES_DIR="$( cd "$( dirname "$0" )" && pwd)"
 echo "Location of dotfiles directory:"
-echo $DOTFILES
+echo $DOTFILES_DIR
 echo "Location of home directory:"
-echo $HOME
+echo $HOME_DIR
 
 # add zsh prompt to oh-my-zsh for autoloading
 cp $DOTFILES_DIR/adh.zsh-theme $DOTFILES_DIR/oh-my-zsh/themes/.
 
 # bash config
-ln -s $DOTFILES_DIR/bash_aliases $HOME/.bash_aliases
-ln -s $DOTFILES_DIR/bash_logout $HOME/.bash_logout
-ln -s $DOTFILES_DIR/bashrc $HOME/.bashrc
+ln -s $DOTFILES_DIR/bash_aliases $HOME_DIR/.bash_aliases
+ln -s $DOTFILES_DIR/bash_logout $HOME_DIR/.bash_logout
+ln -s $DOTFILES_DIR/bashrc $HOME_DIR/.bashrc
 
 # terminator config
-ln -s $DOTFILES_DIR/terminator_config $HOME/.config/terminator/config
+ln -s $DOTFILES_DIR/terminator_config $HOME_DIR/.config/terminator/config
 
 # tmux config
-ln -s $DOTFILES_DIR/tmux.conf $HOME/.tmux.conf
+ln -s $DOTFILES_DIR/tmux.conf $HOME_DIR/.tmux.conf
 
 # zsh config
-ln -s $DOTFILES_DIR/zsh_aliases $HOME/.zsh_aliases
-ln -s $DOTFILES_DIR/zshrc $HOME/.zshrc
+ln -s $DOTFILES_DIR/zsh_aliases $HOME_DIR/.zsh_aliases
+ln -s $DOTFILES_DIR/zshrc $HOME_DIR/.zshrc
 
 echo -e "Set up of dotfiles complete."
